635121beaa1f6edde99fddb13ceeef4b84191178
isethguy <isethguy@gmail.com>
2016-02-05 14:05:47 -0500

    function

--- itis0.js
+++ itis0.js
@@ -1,1 +1,25 @@
+var itis = function(){
+
+this.lray = function( ray , x  ){
+
+var l_ = [ new Array() ];
+
+for( var i = 0; i < ray.length; i++) {
+
+l_[ l_.length-1 ].push( ray[i] );
+
+if( (i+1)%x==0 ){
+
+if( i < ray.length-1 )l_.push( new Array() );
+
+}
+
+}// loop
+
+return l_;
+}//lray
+
+
+}//itis
+
 
